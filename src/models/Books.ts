7d0831bf30b841931a5ecab5310f67bb538f5949
Joel <2016014076@unfv.edu.pe>
2023-05-03 00:37:39 -0500

    Create Book and Rating models

---
+++ src/models/Books.ts
@@ -0,0 +1,18 @@
+export interface Book {
+    id: string
+    ratings: Rating[]
+    title: string
+    author: string
+    year_publication: string
+    publisher: string
+    image_1: string
+    image_2: string
+    image_3: string
+}
+
+export interface Rating {
+    id: number
+    rating: number
+    user: string
+    book: string
+}
