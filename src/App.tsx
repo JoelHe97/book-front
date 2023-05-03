bdd8dbe9ef986a9c5780333a8fe39644cd4d09c9
Joel <2016014076@unfv.edu.pe>
2023-05-03 00:36:26 -0500

    Set routes with react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route, Outlet, Link, useNavigate } from 'react-router-dom'
 import './App.css'
-
+import Home from './pages/Home'
+const NotImplemented = () => {
+  return <>
+    <Link to="/books">Ir a libros</Link>
+    <h1>Esta página no está lista</h1>
+  </>
+}
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='*' element={<NotImplemented />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
