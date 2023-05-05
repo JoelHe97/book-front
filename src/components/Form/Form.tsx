bc37723869099b4b41e912db0ff5cef5ed73f2fd
Joel <2016014076@unfv.edu.pe>
2023-05-05 01:13:00 -0500

    Add default values in form Use reset method to set api's data in fields

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,5 @@
 import { Button, TextField } from "@mui/material";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { Book } from '../../models/Books';
 import { useEffect, useState } from 'react';
 import { books } from "../../api/books.api";
 
@@ -20,41 +19,32 @@ export type ModalProps = {
 }
 const Form: React.FC<ModalProps> = ({ id }) => {
 	const onSubmit: SubmitHandler<Inputs> = (data) => {
-		console.log(data)
 		if (id) {
-			books.updateBook(book)
+			books.updateBook(data)
 		}
 	}
-	const [book, setBook] = useState<Book>({
-		title: "",
-		author: "",
-		publisher: "",
-		year_publication: "",
-		image_1: "",
-		image_2: "",
-		image_3: "",
-	});
 
-	const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>({
-		defaultValues: book
-	});
+
+	const { register, handleSubmit, reset, formState: { errors } } = useForm<Inputs>();
 	useEffect(() => {
 		if (id) {
 			books.getBook(id).then(r => {
-				setBook(r.data)
+				reset(r.data)
 			}).catch(console.error)
 
 		}
-	}, [id])
+	}, [reset, id])
 
 	return (
 		/* "handleSubmit" will validate your inputs before invoking "onSubmit" */
 		<form onSubmit={handleSubmit(onSubmit)}>
 			<div style={{ display: "grid", gridGap: "1em" }}>
-				<TextField  {...register("title", { required: true, value: book.title })}
+				<TextField  {...register("title", { required: true })}
 					id="outlined-password-input"
 					label="Title"
+					defaultValue={id ? "..." : ""}
 					type="text"
+
 				/>
 				{errors.title && <span>This field is required</span>}
 				{/* errors will return when field validation fails  */}
@@ -62,8 +52,8 @@ const Form: React.FC<ModalProps> = ({ id }) => {
 				<TextField  {...register("author", { required: true })}
 					id="outlined-password-input"
 					label="Author"
+					defaultValue={id ? "..." : ""}
 					type="text"
-					value={book?.author}
 
 				/>
 				{errors.author && <span>This field is required</span>}
@@ -71,8 +61,8 @@ const Form: React.FC<ModalProps> = ({ id }) => {
 				<TextField  {...register("publisher", { required: true })}
 					id="outlined-password-input"
 					label="Publisher"
+					defaultValue={id ? "..." : ""}
 					type="text"
-					value={book?.publisher}
 
 				/>
 				{errors.publisher && <span>This field is required</span>}
@@ -80,8 +70,8 @@ const Form: React.FC<ModalProps> = ({ id }) => {
 				<TextField  {...register("year_publication", { required: true })}
 					id="outlined-password-input"
 					label="Year publication"
+					defaultValue={id ? "..." : ""}
 					type="text"
-					value={book?.year_publication}
 
 				/>
 				{errors.year_publication && <span>This field is required</span>}
@@ -89,24 +79,24 @@ const Form: React.FC<ModalProps> = ({ id }) => {
 				<TextField  {...register("image_1", { required: true })}
 					id="outlined-password-input"
 					label="Image 1"
+					defaultValue={id ? "..." : ""}
 					type="text"
-					value={book?.image_1}
 
 				/>
 				{errors.image_1 && <span>This field is required</span>}
 				<TextField  {...register("image_2", { required: true })}
 					id="outlined-password-input"
 					label="Image 2"
+					defaultValue={id ? "..." : ""}
 					type="text"
-					value={book?.image_2}
 
 				/>
 				{errors.image_2 && <span>This field is required</span>}
 				<TextField  {...register("image_3", { required: true })}
 					id="outlined-password-input"
 					label="Image 3"
+					defaultValue={id ? "..." : ""}
 					type="text"
-					value={book?.image_3}
 
 				/>
 				{errors.image_3 && <span>This field is required</span>}
